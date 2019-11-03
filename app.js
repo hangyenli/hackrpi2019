aa3d4f8e504f587327b1e3ec3ca6d02a88b22ca8
hangyenli <lihonghao2010@gmail.com>
2019-11-03 11:23:35 -0500

    finish up the demo

--- app.js
+++ app.js
@@ -5,24 +5,32 @@ app.use(express.static('public'));
 let port = 3000;
 
 app.get('/', (req, res) => res.sendfile('public/demo.html'));
-app.get('/rod', (req, res) => res.sendfile('public/RodAnimation.html'));
+app.get('/rod', (req, res) => res.sendfile('public/rod.html'));
 app.get('/animal', (req, res) => res.sendfile('public/animal.html'));
-app.get('/data', (req, res) => res.sendfile('public/animal.html'));
+app.get('/data', (req, res) => res.sendfile('public/data.html'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://honghao:li988168@cluster0-bze0a.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {useNewUrlParser: true});
-client.connect(err => {
-    const collection = client.db("hackrpi").collection("animal");
-    // perform actions on the collection object
-    // collection.insertMany([
-    //     {a: 1}, {a: 2}, {a: 3}
-    // ], function (err, result) {
-    //
-    //     console.log("Inserted 3 documents into the collection");
-    //     console.log(result);
-    // });
-    client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://honghao:@cluster0-bze0a.mongodb.net/test?retryWrites=true&w=majority";
+
+//userd for transform data
+// const client = new MongoClient(uri, {useNewUrlParser: true});
+//
+// const csvFilePath='model/weather.csv';
+// const csv=require('csvtojson');
+// csv()
+//     .fromFile(csvFilePath)
+//     .then((jsonObj)=>{
+//         console.log(jsonObj);
+//         client.connect(err => {
+//             const collection = client.db("hackrpi").collection("pastdata");
+//             // perform actions on the collection object
+//             collection.insertMany(jsonObj, function (err, result) {
+//
+//                 console.log("Inserted 3 documents into the collection");
+//                 console.log(result);
+//             });
+//             client.close();
+//         });
+//     });
